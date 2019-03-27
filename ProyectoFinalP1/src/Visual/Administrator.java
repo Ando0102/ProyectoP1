169b82707a60f86108a30d2aabf1627052b3e873
Armando Román <48103702+Ando0102@users.noreply.github.com>
2019-03-27 06:03:05 -0400

    Aqui les subi algunas imagenes para las interfaces y tambien la interfaz de administrador

---
+++ ProyectoFinalP1/src/Visual/Administrator.java
@@ -0,0 +1,154 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenuItem;
+import java.awt.Toolkit;
+import javax.swing.border.BevelBorder;
+import java.awt.Color;
+
+public class Administrator extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtHoraYFecha;
+
+	
+	public Administrator() {
+		setTitle("Administrador");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Administrator.class.getResource("/Imagenes/LogoPeque.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1042, 503);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Registrar");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnDoctor = new JMenu("Doctor");
+		mnNewMenu.add(mnDoctor);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Agregar Doctor");
+		mnDoctor.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Doctores Existentes");
+		mnDoctor.add(mntmNewMenuItem_1);
+		
+		JMenu mnSecretaria = new JMenu("Secretaria");
+		mnNewMenu.add(mnSecretaria);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Agregar Secretaria");
+		mnSecretaria.add(mntmNewMenuItem_2);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Secretarias Existentes");
+		mnSecretaria.add(mntmNewMenuItem_3);
+		
+		JMenu mnNewMenu_1 = new JMenu("Estadisticas");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmEmpleados = new JMenuItem("Empleados");
+		mnNewMenu_1.add(mntmEmpleados);
+		
+		JMenuItem mntmNewMenuItem_8 = new JMenuItem("Pacientes");
+		mnNewMenu_1.add(mntmNewMenuItem_8);
+		
+		JMenuItem mntmNewMenuItem_9 = new JMenuItem("Consultas");
+		mnNewMenu_1.add(mntmNewMenuItem_9);
+		
+		JMenuItem mntmNewMenuItem_10 = new JMenuItem("Enfermedades");
+		mnNewMenu_1.add(mntmNewMenuItem_10);
+		
+		JMenuItem mntmVacunas = new JMenuItem("Vacunas");
+		mnNewMenu_1.add(mntmVacunas);
+		
+		JMenu mnNewMenu_2 = new JMenu("Enfermedades");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Nueva Enfermedad");
+		mnNewMenu_2.add(mntmNewMenuItem_4);
+		
+		JMenuItem mntmNewMenuItem_5 = new JMenuItem("Enfermedades Existentes");
+		mnNewMenu_2.add(mntmNewMenuItem_5);
+		
+		JMenu mnNewMenu_3 = new JMenu("Vacunas");
+		menuBar.add(mnNewMenu_3);
+		
+		JMenuItem mntmNewMenuItem_6 = new JMenuItem("Nueva Vacuna");
+		mnNewMenu_3.add(mntmNewMenuItem_6);
+		
+		JMenuItem mntmNewMenuItem_7 = new JMenuItem("Vacunas Existentes");
+		mnNewMenu_3.add(mntmNewMenuItem_7);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		panel_1.setBounds(10, 11, 199, 411);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/man.png")));
+		lblNewLabel.setBounds(10, 11, 89, 129);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblBienvenidoa = new JLabel("Bienvenido/a ");
+		lblBienvenidoa.setBounds(55, 151, 89, 49);
+		panel_1.add(lblBienvenidoa);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/woman.png")));
+		lblNewLabel_1.setBounds(109, 11, 80, 129);
+		panel_1.add(lblNewLabel_1);
+		
+		txtHoraYFecha = new JTextField();
+		txtHoraYFecha.setBackground(Color.WHITE);
+		txtHoraYFecha.setEditable(false);
+		txtHoraYFecha.setText("          HORA Y FECHA\r\n");
+		txtHoraYFecha.setBounds(10, 296, 179, 104);
+		panel_1.add(txtHoraYFecha);
+		txtHoraYFecha.setColumns(10);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		panel_2.setBounds(219, 11, 787, 411);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+}
