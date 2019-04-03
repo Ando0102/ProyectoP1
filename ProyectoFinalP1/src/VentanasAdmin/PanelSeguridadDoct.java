813a52f553573952dc9b2e07857dee7c6f8dd958
Armando Román <48103702+Ando0102@users.noreply.github.com>
2019-04-03 04:25:26 -0400

    Modificaciones a VentanaDoctor_admin

--- ProyectoFinalP1/src/VentanasAdmin/PanelSeguridadDoct.java
+++ ProyectoFinalP1/src/VentanasAdmin/PanelSeguridadDoct.java
@@ -7,6 +7,8 @@ import java.awt.event.MouseEvent;
 import java.util.Calendar;
 
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -19,54 +21,19 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 
+import com.toedter.calendar.JDateChooser;
+
 import Logical.Clinica;
 import Logical.Doctor;
 import Logical.Persona;
 
-public class PanelSeguridadDoct extends JPanel {
+public class PanelSeguridadDoct extends JDialog {
 
 	private JTextField txtUsuario;
 	private JPasswordField passwordField1;
 	private JPasswordField passwordField2;
 
-	/**
-	 * Create the panel.
-	 */
-	PanelSeguridadDoct() {
-	
-	setBorder(new TitledBorder(null, "Informaci\u00F3n del Nuevo Doctor", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-	setBackground(new Color(230, 230, 250));
-	setBounds(0, 0, 764, 413);
-	setVisible(false);
-	setLayout(null);
-	/*
-	JLabel lblNewLabel = new JLabel("");
-	lblNewLabel.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/LogoGrande.png")));
-	lblNewLabel.setBounds(10, 40, 128, 161);
-	//PanelSeguridadDoct.add(lblNewLabel);
-	
-	JLabel lblNewLabel_1 = new JLabel("");
-	lblNewLabel_1.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/Escudo.png")));
-	lblNewLabel_1.setBounds(639, 40, 115, 161);
-	//panelSeguridad.add(lblNewLabel_1);
-	
-	JLabel btnAtras = new JLabel("");
-	btnAtras.addMouseListener(new MouseAdapter() {
-		@Override
-		public void mouseClicked(MouseEvent arg0) {
-			
-		}
-	});
-	btnAtras.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/atraspeque\u00F1a.png")));
-	btnAtras.setBackground(UIManager.getColor("Button.highlight"));
-	btnAtras.setBounds(639, 368, 53, 34);
-	//panelSeguridad.add(btnAtras);
-	
-	JTextPane txtpnLasPartesExponen = new JTextPane();
-	txtpnLasPartesExponen.setBackground(new Color(230, 230, 250));	
-	StyledDocument doc = txtpnLasPartesExponen.getStyledDocument();
-	
-	SimpleAttributeSet keyWord = new SimpleAttributeSet();
+	/*SimpleAttributeSet keyWord = new SimpleAttributeSet();
 	StyleConstants.setForeground(keyWord, Color.BLACK);
 	StyleConstants.setBold(keyWord, true);
 
@@ -76,104 +43,16 @@ public class PanelSeguridadDoct extends JPanel {
 	    doc.insertString(0, "Estimado Doctor/ar. Por este medio se le informa que la siguiente información que usted mismo suministrará a continuación, es de total confidencialidad para su persona. Es su deber mantener en secreto y no divulgar u compartir con nadie está información, sin importar que a las personas las cuales usted desea compartirla sean colegas o superiores de usted.  Absolutamente nadie debe de saber está información a demas de usted. Es su deber como profesional y miembro de está institución cumplir esta normal." , null );
 	    doc.insertString(doc.getLength(), "\n", null );
 	}
-	catch(Exception e) { System.out.println(e); }
+	catch(Exception e) { System.out.println(e); }/**
+	 * Create the panel.
+	 */
+	public	PanelSeguridadDoct(JTextField txtApellido, JTextField txtCedula, JTextField txtCorreoElect, JTextField txtDireccion, JTextField txtNombre, JTextField txtTelefono, JComboBox cbxGenero, JComboBox cbxPais, JDateChooser dcFechaNacimiento) {
 	
-	txtpnLasPartesExponen.setFont(new Font("Tahoma", Font.PLAIN, 12));
-	txtpnLasPartesExponen.setBounds(178, 51, 411, 150);
-	//panelSeguridad.add(txtpnLasPartesExponen);
+	setBackground(new Color(230, 230, 250));
+	setBounds(0, 0, 764, 413);
+	setLayout(null);
 	
-	JLabel lblUsuarioParaIniciar = new JLabel("Usuario para Iniciar Sesi\u00F3n :");
-	lblUsuarioParaIniciar.setBounds(269, 211, 162, 14);
-	//panelSeguridad.add(lblUsuarioParaIniciar);
-	
-	txtUsuario = new JTextField();
-	txtUsuario.setBounds(279, 236, 150, 20);
-	//panelSeguridad.add(txtUsuario);
-	txtUsuario.setColumns(10);
-	
-	JLabel lblContrasea = new JLabel("Contrase\u00F1a :");
-	lblContrasea.setBounds(269, 267, 100, 14);
-	//panelSeguridad.add(lblContrasea);
-	
-	passwordField1 = new JPasswordField();
-	passwordField1.setBounds(279, 292, 152, 20);
-	//panelSeguridad.add(passwordField1);
-	
-	JLabel lblConfimacinDeContrasea = new JLabel("Confimaci\u00F3n de Contrase\u00F1a :");
-	lblConfimacinDeContrasea.setBounds(269, 323, 179, 14);
-	//panelSeguridad.add(lblConfimacinDeContrasea);
-	
-	passwordField2 = new JPasswordField();
-	passwordField2.setBounds(279, 352, 152, 20);
-	//panelSeguridad.add(passwordField2);
-	JLabel lblNewLabel_4 = new JLabel("");
-	lblNewLabel_4.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/acceptar.png")));
-	lblNewLabel_4.setBounds(693, 367, 71, 35);
-	//panelSeguridad.add(lblNewLabel_4);
-	
-	
-	
-///////////////////////////////////////////////////////////CREANDO NUEVO DOCTOR////////////////////////////////////////////////////////		
-/*	
-			lblNewLabel_4.addMouseListener(new MouseAdapter() {
-			
-			
-			
-			
-			@SuppressWarnings("null")
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			String nombre, cedula, apellido, correo, direccion, telefono, nacionalidad, usuario, contraseña = null;
-			boolean sexo = false;
-			//JCalendar fecha_de_nacimiento = JCalendar.get
-			Calendar fecha_de_nacimiento  = Calendar.getInstance();
-			//elimine el anno
-			if((dcFechaNacimiento.getDate()!=null)&&!txtNombre.getText().equalsIgnoreCase("") && !txtApellido.getText().equalsIgnoreCase("") && !txtCorreoElect.getText().equalsIgnoreCase("") && !txtDireccion.getText().equalsIgnoreCase("") && cbxGenero.getSelectedIndex() != 0 &&
-			!txtCedula.getText().equalsIgnoreCase("") && !txtTelefono.getText().equalsIgnoreCase("") && !txtUsuario.getText().equalsIgnoreCase("") && !passwordField1.getText().equalsIgnoreCase("") && !passwordField2.getText().equalsIgnoreCase("") && cbxPais.getSelectedIndex() != 0) {
-			if(passwordField1.getText().equalsIgnoreCase(passwordField2.getText())) {
-			
-			nombre = txtNombre.getText();
-			apellido = txtApellido.getText();
-			cedula = txtCedula.getText();
-			correo = txtCorreoElect.getText();
-			telefono = txtTelefono.getText();
-			direccion = txtDireccion.getText();
-			usuario = txtUsuario.getText();
-			contraseña = passwordField2.getText();
-			nacionalidad = cbxPais.getSelectedItem().toString();
-			//aqui esta el asuntofecha_de_nacimiento.set(Integer.parseInt(txtanos.getText()), cbxMeses.getSelectedIndex(), Integer.parseInt(spinDias.getValue().toString()));
-			
-			
-			//pasanddo la fecha seleccionada
-			fecha_de_nacimiento.setTime(dcFechaNacimiento.getDate());
-			if(cbxGenero.getSelectedItem().toString().equalsIgnoreCase("Masculino")) {
-			sexo = true;
-			}else if(cbxGenero.getSelectedItem().toString().equalsIgnoreCase("Femenino")) {
-			sexo = false;
-			}
-			
-			///Clinica.getInstance().get
-			///	Doctor aux = new LOGICO.Persona(nombre, apellido, cedula, correo, fecha_nacimiento);
-			Persona aux = new Doctor("CD- "+Integer.toString(cantidadDoctores), nombre, apellido, cedula, sexo, telefono, nacionalidad, fecha_de_nacimiento, correo, usuario, contraseña, "Doctor");
-			Clinica.getInstance().insertarPersona(aux);
-			
-			//no esto seguro que esta aparte funcione sobre cantida de doctores
-			cantidadDoctores = cantidadDoctores + 1;
-			JOptionPane.showMessageDialog(null, "Nuevo Doctor/ar Agregado", "Información", JOptionPane.INFORMATION_MESSAGE, null);
-			dispose();	
-			
-			}else {
-			JOptionPane.showMessageDialog(null, "Las Contraseñas no Coinciden", "Advertencia", JOptionPane.WARNING_MESSAGE, null);
-			}
-			
-			
-			}else {
-			JOptionPane.showMessageDialog(null, "Por favor, Completar Todos los Campos", "Advertencia", JOptionPane.WARNING_MESSAGE, null);
-			}
-			
-			
-			}
-			});
-*/
-}
+	}
+
+
 }
--- ProyectoFinalP1/src/VentanasAdmin/VentanaDoctor_admin.java
+++ ProyectoFinalP1/src/VentanasAdmin/VentanaDoctor_admin.java
@@ -49,16 +49,12 @@ import com.toedter.calendar.JDateChooser;
 public class VentanaDoctor_admin extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField txtNombre;
-	private JTextField txtApellido;
-	private JTextField txtCedula;
-	private JTextField txtTelefono;
-	private JTextField txtDireccion;
-	private JTextField txtCorreoElect;
-	private JTextField txtUsuario;
-	private JPasswordField passwordField1;
-	private JPasswordField passwordField2;
-	private JPanel panelSeguridad;
+	public JTextField txtNombre;
+	public JTextField txtApellido;
+	public JTextField txtCedula;
+	public JTextField txtTelefono;
+	public JTextField txtDireccion;
+	public JTextField txtCorreoElect;
 	private JPanel panel_2;
 	private JTextField textField_8;
 	private JTextField textField_9;
@@ -66,8 +62,9 @@ public class VentanaDoctor_admin extends JDialog {
 	private JTextField textField_11;
 	private JTextField textField_12;
 	private JLabel btnSiguiente;
-	private int cantidadDoctores = 0;
-	private JDateChooser dcFechaNacimiento;
+	public JDateChooser dcFechaNacimiento;
+	public JComboBox cbxPais;
+	public JComboBox cbxGenero;
 	/**
 	 * Launch the application.
 	 */
@@ -88,7 +85,7 @@ public class VentanaDoctor_admin extends JDialog {
 		setModal(true);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(VentanaDoctor_admin.class.getResource("/Imagenes/LogoPeque.png")));
 		setTitle("Informaci\u00F3n de Doctor");
-		setBounds(100, 100, 798, 494);
+		setBounds(100, 100, 668, 551);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(new Color(240, 248, 255));
@@ -105,266 +102,130 @@ public class VentanaDoctor_admin extends JDialog {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBackground(new Color(230, 230, 250));
 		panel.add(tabbedPane, BorderLayout.CENTER);
-
-		//  Define a keyword attribute
-
 		
+				//  Define a keyword attribute
 		
-		panel_2 = new JPanel();
-		panel_2.setBackground(new Color(230, 230, 250));
-		panel_2.setLayout(null);
-		panel_2.setBorder(new TitledBorder(null, "Informaci\u00F3n del Nuevo Doctor", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		tabbedPane.addTab("Agregar Nuevo Doctor", null, panel_2, null);
-		
-		panelSeguridad = new JPanel();
-		panelSeguridad.setBorder(new TitledBorder(null, "Informaci\u00F3n del Nuevo Doctor", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelSeguridad.setBackground(new Color(230, 230, 250));
-		panelSeguridad.setBounds(0, 0, 764, 413);
-		panelSeguridad.setVisible(false);
-		panel_2.add(panelSeguridad);
-		panelSeguridad.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/LogoGrande.png")));
-		lblNewLabel.setBounds(10, 40, 128, 161);
-		panelSeguridad.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/Escudo.png")));
-		lblNewLabel_1.setBounds(639, 40, 115, 161);
-		panelSeguridad.add(lblNewLabel_1);
-		
-		JLabel btnAtras = new JLabel("");
-		btnAtras.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				panel_2.setVisible(true);
-				panelSeguridad.setVisible(false);
-			}
-		});
-		btnAtras.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/atraspeque\u00F1a.png")));
-		btnAtras.setBackground(UIManager.getColor("Button.highlight"));
-		btnAtras.setBounds(639, 368, 53, 34);
-		panelSeguridad.add(btnAtras);
-		
-		JTextPane txtpnLasPartesExponen = new JTextPane();
-		txtpnLasPartesExponen.setBackground(new Color(230, 230, 250));	
-		StyledDocument doc = txtpnLasPartesExponen.getStyledDocument();
-		
-		SimpleAttributeSet keyWord = new SimpleAttributeSet();
-		StyleConstants.setForeground(keyWord, Color.BLACK);
-		StyleConstants.setBold(keyWord, true);
-
-
-		try
-		{
-		    doc.insertString(0, "Estimado Doctor/ar. Por este medio se le informa que la siguiente información que usted mismo suministrará a continuación, es de total confidencialidad para su persona. Es su deber mantener en secreto y no divulgar u compartir con nadie está información, sin importar que a las personas las cuales usted desea compartirla sean colegas o superiores de usted.  Absolutamente nadie debe de saber está información a demas de usted. Es su deber como profesional y miembro de está institución cumplir esta normal." , null );
-		    doc.insertString(doc.getLength(), "\n", null );
-		}
-		catch(Exception e) { System.out.println(e); }
-		
-		txtpnLasPartesExponen.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		txtpnLasPartesExponen.setBounds(178, 51, 411, 150);
-		panelSeguridad.add(txtpnLasPartesExponen);
-		
-		JLabel lblUsuarioParaIniciar = new JLabel("Usuario para Iniciar Sesi\u00F3n :");
-		lblUsuarioParaIniciar.setBounds(269, 211, 162, 14);
-		panelSeguridad.add(lblUsuarioParaIniciar);
-		
-		txtUsuario = new JTextField();
-		txtUsuario.setBounds(279, 236, 150, 20);
-		panelSeguridad.add(txtUsuario);
-		txtUsuario.setColumns(10);
-		
-		JLabel lblContrasea = new JLabel("Contrase\u00F1a :");
-		lblContrasea.setBounds(269, 267, 100, 14);
-		panelSeguridad.add(lblContrasea);
-		
-		passwordField1 = new JPasswordField();
-		passwordField1.setBounds(279, 292, 152, 20);
-		panelSeguridad.add(passwordField1);
-		
-		JLabel lblConfimacinDeContrasea = new JLabel("Confimaci\u00F3n de Contrase\u00F1a :");
-		lblConfimacinDeContrasea.setBounds(269, 323, 179, 14);
-		panelSeguridad.add(lblConfimacinDeContrasea);
-		
-		passwordField2 = new JPasswordField();
-		passwordField2.setBounds(279, 352, 152, 20);
-		panelSeguridad.add(passwordField2);
-		JLabel lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/acceptar.png")));
-		lblNewLabel_4.setBounds(693, 367, 71, 35);
-		panelSeguridad.add(lblNewLabel_4);
-		
-		JLabel label = new JLabel("Nombres :");
-		label.setBounds(10, 28, 72, 22);
-		panel_2.add(label);
-		
-		JLabel label_1 = new JLabel("Apellidos :");
-		label_1.setBounds(192, 32, 93, 18);
-		panel_2.add(label_1);
-		
-		JLabel label_2 = new JLabel("Cedula :");
-		label_2.setBounds(396, 32, 64, 18);
-		panel_2.add(label_2);
-		
-		JLabel label_3 = new JLabel("Tel\u00E9fono :");
-		label_3.setBounds(10, 98, 72, 18);
-		panel_2.add(label_3);
-		
-		JLabel label_4 = new JLabel("Direcci\u00F3n :");
-		label_4.setBounds(192, 99, 72, 16);
-		panel_2.add(label_4);
-		
-		JLabel label_5 = new JLabel("G\u00E9nero :");
-		label_5.setBounds(10, 183, 72, 14);
-		panel_2.add(label_5);
-		
-		JLabel label_6 = new JLabel("Nacionalidad :");
-		label_6.setBounds(187, 183, 98, 20);
-		panel_2.add(label_6);
-		
-		JLabel label_7 = new JLabel("Correo El\u00E9ctronico :");
-		label_7.setBounds(10, 252, 145, 20);
-		panel_2.add(label_7);
-		
-		JLabel label_8 = new JLabel("Fecha de Nacimiento :");
-		label_8.setBounds(10, 328, 125, 14);
-		panel_2.add(label_8);
-		
-		JLabel label_12 = new JLabel("");
-		label_12.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/doctor.png")));
-		label_12.setBounds(605, 238, 99, 103);
-		panel_2.add(label_12);
-		
-		btnSiguiente = new JLabel("");
-		btnSiguiente.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/arrowPeque\u00F1a.png")));
-		btnSiguiente.setBounds(690, 368, 64, 34);
-		btnSiguiente.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				panel_2.setVisible(false);
-				panelSeguridad.setVisible(true);
-			}
-		});
-		panel_2.add(btnSiguiente);
-		
-		JLabel label_13 = new JLabel("");
-		label_13.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/doctora.png")));
-		label_13.setBounds(512, 238, 93, 109);
-		panel_2.add(label_13);
-		
-		txtTelefono = new JTextField();
-		txtTelefono.setColumns(10);
-		txtTelefono.setBounds(20, 127, 135, 20);
-		panel_2.add(txtTelefono);
-		
-		txtCedula = new JTextField();
-		txtCedula.setColumns(10);
-		txtCedula.setBounds(406, 56, 135, 20);
-		panel_2.add(txtCedula);
-		
-		txtApellido = new JTextField();
-		txtApellido.setColumns(10);
-		txtApellido.setBounds(202, 56, 135, 20);
-		panel_2.add(txtApellido);
-		
-		final JComboBox cbxPais = new JComboBox();
-		cbxPais.setBackground(new Color(240, 248, 255));
-		cbxPais.setMaximumRowCount(5);
-		cbxPais.setModel(new DefaultComboBoxModel(new String[] {"<Pa\u00EDs>", "Alemania", "Argentina", "Australia", "Burgar\u00EDa", "Canad\u00E1", "Colombia", "Cuba", "Hait\u00ED", "Mexico", "Rep. Dominicana", "Rusia", "Rep. Checa", "Venezuela", "Jap\u00F3n"}));
-		cbxPais.setSelectedIndex(0);
-		cbxPais.setBounds(202, 208, 135, 20);
-		panel_2.add(cbxPais);
-		
-		txtDireccion = new JTextField();
-		txtDireccion.setColumns(10);
-		txtDireccion.setBounds(202, 127, 340, 20);
-		panel_2.add(txtDireccion);
-		
-		dcFechaNacimiento = new JDateChooser();
-		dcFechaNacimiento.setBounds(20, 355, 103, 20);
-		panel_2.add(dcFechaNacimiento);
-		
-		txtNombre = new JTextField();
-		txtNombre.setColumns(10);
-		txtNombre.setBounds(20, 56, 135, 20);
-		panel_2.add(txtNombre);
-		
-		final JComboBox cbxGenero = new JComboBox();
-		cbxGenero.setBackground(new Color(240, 248, 255));
-		cbxGenero.setModel(new DefaultComboBoxModel(new String[] {"<G\u00E9nero>", "Masculino", "Femenino"}));
-		cbxGenero.setSelectedIndex(0);
-		cbxGenero.setBounds(20, 208, 93, 20);
-		panel_2.add(cbxGenero);
-		
-		txtCorreoElect = new JTextField();
-		txtCorreoElect.setColumns(10);
-		txtCorreoElect.setBounds(20, 283, 317, 20);
-		panel_2.add(txtCorreoElect);
-		
-		
-///////////////////////////////////////////////////////////CREANDO NUEVO DOCTOR////////////////////////////////////////////////////////		
-		
-		lblNewLabel_4.addMouseListener(new MouseAdapter() {
-			
-
-			
-
-			@SuppressWarnings("null")
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				String nombre, cedula, apellido, correo, direccion, telefono, nacionalidad, usuario, contraseña = null;
-				boolean sexo = false;
-				//JCalendar fecha_de_nacimiento = JCalendar.get
-				Calendar fecha_de_nacimiento  = Calendar.getInstance();
-				//elimine el anno
-				if((dcFechaNacimiento.getDate()!=null)&&!txtNombre.getText().equalsIgnoreCase("") && !txtApellido.getText().equalsIgnoreCase("") && !txtCorreoElect.getText().equalsIgnoreCase("") && !txtDireccion.getText().equalsIgnoreCase("") && cbxGenero.getSelectedIndex() != 0 &&
-						!txtCedula.getText().equalsIgnoreCase("") && !txtTelefono.getText().equalsIgnoreCase("") && !txtUsuario.getText().equalsIgnoreCase("") && !passwordField1.getText().equalsIgnoreCase("") && !passwordField2.getText().equalsIgnoreCase("") && cbxPais.getSelectedIndex() != 0) {
-					if(passwordField1.getText().equalsIgnoreCase(passwordField2.getText())) {
+				
+				
+				panel_2 = new JPanel();
+				panel_2.setBackground(new Color(230, 230, 250));
+				panel_2.setLayout(null);
+				panel_2.setBorder(new TitledBorder(null, "Informaci\u00F3n del Nuevo Doctor", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				tabbedPane.addTab("Agregar Nuevo Doctor", null, panel_2, null);
+				
+				JLabel label_5 = new JLabel("");
+				label_5.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/plus_15650.png")));
+				label_5.setBounds(468, 134, 64, 74);
+				panel_2.add(label_5);
+				
+				JLabel label = new JLabel("Nombres :");
+				label.setBounds(29, 28, 72, 22);
+				panel_2.add(label);
+				
+				JLabel label_1 = new JLabel("Apellidos :");
+				label_1.setBounds(234, 28, 93, 18);
+				panel_2.add(label_1);
+				
+				JLabel label_2 = new JLabel("Cedula :");
+				label_2.setBounds(234, 83, 64, 18);
+				panel_2.add(label_2);
+				
+				JLabel label_3 = new JLabel("Tel\u00E9fono :");
+				label_3.setBounds(29, 83, 72, 18);
+				panel_2.add(label_3);
+				
+				JLabel label_4 = new JLabel("Direcci\u00F3n :");
+				label_4.setBounds(29, 142, 72, 16);
+				panel_2.add(label_4);
+				
+				JLabel lblSexo = new JLabel("Sexo :");
+				lblSexo.setBounds(29, 272, 72, 14);
+				panel_2.add(lblSexo);
+				
+				JLabel lblPasDeOrigen = new JLabel("Pa\u00EDs de Origen :");
+				lblPasDeOrigen.setBounds(206, 272, 98, 20);
+				panel_2.add(lblPasDeOrigen);
+				
+				JLabel label_7 = new JLabel("Correo El\u00E9ctronico :");
+				label_7.setBounds(29, 200, 145, 20);
+				panel_2.add(label_7);
+				
+				JLabel label_8 = new JLabel("Fecha de Nacimiento :");
+				label_8.setBounds(29, 333, 125, 14);
+				panel_2.add(label_8);
+				
+				JLabel label_12 = new JLabel("");
+				label_12.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/doctor.png")));
+				label_12.setBounds(498, 83, 99, 103);
+				panel_2.add(label_12);
+				
+				
+				JLabel label_13 = new JLabel("");
+				label_13.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/doctora.png")));
+				label_13.setBounds(429, 80, 93, 109);
+				panel_2.add(label_13);
+				
+				txtTelefono = new JTextField();
+				txtTelefono.setColumns(10);
+				txtTelefono.setBounds(29, 107, 135, 20);
+				panel_2.add(txtTelefono);
+				
+				txtCedula = new JTextField();
+				txtCedula.setColumns(10);
+				txtCedula.setBounds(234, 107, 135, 20);
+				panel_2.add(txtCedula);
+				
+				txtApellido = new JTextField();
+				txtApellido.setColumns(10);
+				txtApellido.setBounds(234, 52, 135, 20);
+				panel_2.add(txtApellido);
+				
+				cbxPais = new JComboBox();
+				cbxPais.setBackground(new Color(240, 248, 255));
+				cbxPais.setMaximumRowCount(5);
+				cbxPais.setModel(new DefaultComboBoxModel(new String[] {"<Pa\u00EDs>", "Alemania", "Argentina", "Australia", "Burgar\u00EDa", "Canad\u00E1", "Colombia", "Cuba", "Hait\u00ED", "Mexico", "Rep. Dominicana", "Rusia", "Rep. Checa", "Venezuela", "Jap\u00F3n"}));
+				cbxPais.setSelectedIndex(0);
+				cbxPais.setBounds(211, 297, 135, 20);
+				panel_2.add(cbxPais);
+				
+				txtDireccion = new JTextField();
+				txtDireccion.setColumns(10);
+				txtDireccion.setBounds(29, 169, 340, 20);
+				panel_2.add(txtDireccion);
+				
+				dcFechaNacimiento = new JDateChooser();
+				dcFechaNacimiento.setBounds(29, 360, 135, 20);
+				panel_2.add(dcFechaNacimiento);
+				
+				txtNombre = new JTextField();
+				txtNombre.setColumns(10);
+				txtNombre.setBounds(29, 56, 135, 20);
+				panel_2.add(txtNombre);
+				
+				cbxGenero = new JComboBox();
+				cbxGenero.setBackground(new Color(240, 248, 255));
+				cbxGenero.setModel(new DefaultComboBoxModel(new String[] {"<Sexo>", "Masculino", "Femenino"}));
+				cbxGenero.setSelectedIndex(0);
+				cbxGenero.setBounds(29, 297, 135, 20);
+				panel_2.add(cbxGenero);
+				
+				txtCorreoElect = new JTextField();
+				txtCorreoElect.setColumns(10);
+				txtCorreoElect.setBounds(29, 231, 340, 20);
+				panel_2.add(txtCorreoElect);
+				
+				JButton btnNewButton = new JButton("Siguiente");
+				btnNewButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
 						
-						 nombre = txtNombre.getText();
-						 apellido = txtApellido.getText();
-						 cedula = txtCedula.getText();
-						 correo = txtCorreoElect.getText();
-						 telefono = txtTelefono.getText();
-						 direccion = txtDireccion.getText();
-						usuario = txtUsuario.getText();
-						contraseña = passwordField2.getText();
-						nacionalidad = cbxPais.getSelectedItem().toString();
-						//aqui esta el asuntofecha_de_nacimiento.set(Integer.parseInt(txtanos.getText()), cbxMeses.getSelectedIndex(), Integer.parseInt(spinDias.getValue().toString()));
-					
-						
-						//pasanddo la fecha seleccionada
-						fecha_de_nacimiento.setTime(dcFechaNacimiento.getDate());
-						if(cbxGenero.getSelectedItem().toString().equalsIgnoreCase("Masculino")) {
-							sexo = true;
-						}else if(cbxGenero.getSelectedItem().toString().equalsIgnoreCase("Femenino")) {
-							sexo = false;
-						}
-						
-						///Clinica.getInstance().get
-						///	Doctor aux = new LOGICO.Persona(nombre, apellido, cedula, correo, fecha_nacimiento);
-						Persona aux = new Doctor("CD- "+Integer.toString(cantidadDoctores), nombre, apellido, cedula, sexo, telefono, nacionalidad, fecha_de_nacimiento, correo, usuario, contraseña, "Doctor");
-						Clinica.getInstance().insertarPersona(aux);
-						
-						//no esto seguro que esta aparte funcione sobre cantida de doctores
-						cantidadDoctores = cantidadDoctores + 1;
-						JOptionPane.showMessageDialog(null, "Nuevo Doctor/ar Agregado", "Información", JOptionPane.INFORMATION_MESSAGE, null);
-						dispose();	
-						
-					}else {
-						 JOptionPane.showMessageDialog(null, "Las Contraseñas no Coinciden", "Advertencia", JOptionPane.WARNING_MESSAGE, null);
+						PanelSeguridadDoct aux = new PanelSeguridadDoct(txtApellido, txtCedula, txtCorreoElect, txtDireccion, txtNombre, txtTelefono, cbxGenero, cbxPais, dcFechaNacimiento);
+						aux.setVisible(true);
+						dispose();
 					}
-					
-					
-				}else {
-					 JOptionPane.showMessageDialog(null, "Por favor, Completar Todos los Campos", "Advertencia", JOptionPane.WARNING_MESSAGE, null);
-				}
-				
-				
-			}
-		});
+				});
+				btnNewButton.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/arrowPeque\u00F1a.png")));
+				btnNewButton.setBounds(496, 425, 127, 34);
+				panel_2.add(btnNewButton);
 		
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(new Color(230, 230, 250));
@@ -374,13 +235,13 @@ public class VentanaDoctor_admin extends JDialog {
 		JPanel panel_4 = new JPanel();
 		panel_4.setBackground(new Color(230, 230, 250));
 		panel_4.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		panel_4.setBounds(10, 172, 744, 230);
+		panel_4.setBounds(10, 213, 613, 246);
 		panel_3.add(panel_4);
 		panel_4.setLayout(null);
 		
 		JLabel lblNewLabel_3 = new JLabel("");
 		lblNewLabel_3.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/Rueda_dentada.png")));
-		lblNewLabel_3.setBounds(484, 132, 83, 72);
+		lblNewLabel_3.setBounds(500, 89, 83, 72);
 		panel_4.add(lblNewLabel_3);
 		
 		JLabel lblNombre = new JLabel("Nombres :");
@@ -430,17 +291,21 @@ public class VentanaDoctor_admin extends JDialog {
 		textField_12.setColumns(10);
 		
 		JButton btnModificar = new JButton("Modificar");
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnModificar.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/edit_pencil_6320 (1).png")));
-		btnModificar.setBounds(604, 176, 130, 41);
+		btnModificar.setBounds(473, 178, 130, 41);
 		panel_4.add(btnModificar);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon(VentanaDoctor_admin.class.getResource("/Imagenes/doctora.png")));
-		lblNewLabel_2.setBounds(502, 87, 76, 96);
+		lblNewLabel_2.setBounds(518, 44, 76, 96);
 		panel_4.add(lblNewLabel_2);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 11, 744, 150);
+		scrollPane.setBounds(10, 11, 613, 191);
 		panel_3.add(scrollPane);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	}
