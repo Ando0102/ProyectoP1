c46ac191d95f6b82d9c66357ca07b94275537e5a
danie <danie@DANIEL-MORONTA>
2019-04-03 23:17:05 -0400

    Nueva funcion para buscar una cita dado:

--- ProyectoFinalP1/src/Logical/Doctor.java
+++ ProyectoFinalP1/src/Logical/Doctor.java
@@ -60,5 +60,37 @@ public class Doctor extends User {
 		}
 		return miCitasSemana;
 	}
+	public Cita buscar_Cita_Fecha(Calendar Fecha, int hora) {
+		//metodo para buscar la cita por la fecha y la hora 
+		Cita tu = null;
+		
+		for (Cita cita :misCitas) {
+			/*
+			System.out.println("-----------------");
+			System.out.println("Dia: "+ cita.getFecha().get(Calendar.DATE));
+			System.out.println("ANNO: "+ cita.getFecha().get(Calendar.YEAR));
+			System.out.println("MES: "+ cita.getFecha().get(Calendar.MONTH));
+			*/
+			System.out.println("cita hora"+cita.getHora());
+			if(cita.getHora()==hora) {
+				System.out.println("Hora conicide");
+				if(cita.getFecha().get(Calendar.DATE)==Fecha.get(Calendar.DATE)
+						&&cita.getFecha().get(Calendar.YEAR)==Fecha.get(Calendar.YEAR)&&
+				cita.getFecha().get(Calendar.MONTH)==Fecha.get(Calendar.MONTH))
+				{
+					tu = cita;
+				}else {
+					//System.out.println("FECHA TA MALA");
+				}
+				
+				}
+			
+		}
+		if(tu==null) {
+			System.out.println("no se puedo encontrar la cita");
+		}
+		return tu;
+	}
+
 
 }
