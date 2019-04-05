9a7368943d0c36a02c26e18563f7025be29aebb3
Armando Román <48103702+Ando0102@users.noreply.github.com>
2019-04-05 00:48:02 -0400

    ventanaEstadistica

---
+++ ProyectoFinalP1/src/VentanasAdmin/VentanaEstadisticas.java
@@ -0,0 +1,64 @@
+package VentanasAdmin;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+
+public class VentanaEstadisticas extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			VentanaEstadisticas dialog = new VentanaEstadisticas();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public VentanaEstadisticas() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(VentanaEstadisticas.class.getResource("/Imagenes/LogoPeque.png")));
+		setTitle("Estad\u00EDsticas Generales");
+		setResizable(false);
+		setModal(true);
+		setBounds(100, 100, 910, 467);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.CENTER);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
