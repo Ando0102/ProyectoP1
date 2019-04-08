5f4f9268565842f12dc17fb205d36f2985094464
Armando Román <48103702+Ando0102@users.noreply.github.com>
2019-04-08 14:37:09 -0400

    ESTADISTICAS AGREGADAS

--- ProyectoFinalP1/src/Visual/Administrator.java
+++ ProyectoFinalP1/src/Visual/Administrator.java
@@ -7,7 +7,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 import Logical.Clinica;
+import Logical.Paciente;
 import VentanasAdmin.VentanaCita_admin;
 import VentanasAdmin.VentanaEnfermedad_admin;
 import VentanasAdmin.VentanaEstadisticas;
@@ -42,6 +50,8 @@ import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JTextPane;
 import javax.swing.UIManager;
+import java.awt.Rectangle;
+import javax.swing.JToolBar;
 
 public class Administrator extends JFrame implements Runnable {
 
@@ -70,6 +80,7 @@ public class Administrator extends JFrame implements Runnable {
 	}
 	
 	public Administrator() {
+		setResizable(false);
 		//metodo prueba
 		//prueba existosa! 
 		/**
@@ -127,7 +138,7 @@ public class Administrator extends JFrame implements Runnable {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(176, 196, 222));
 		panel_1.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		panel_1.setBounds(10, 11, 199, 647);
+		panel_1.setBounds(10, 11, 199, 618);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
@@ -145,7 +156,7 @@ public class Administrator extends JFrame implements Runnable {
 		txtHoraYFecha.setFont(new Font("Tahoma", Font.BOLD, 16));
 		txtHoraYFecha.setBackground(new Color(240, 248, 255));
 		txtHoraYFecha.setEditable(false);
-		txtHoraYFecha.setBounds(10, 532, 179, 104);
+		txtHoraYFecha.setBounds(10, 503, 179, 104);
 		
 		////////HILO PARA EL RELOJ///////////////////////////////////////////
 		Tiempo = new Thread(this);
@@ -153,135 +164,184 @@ public class Administrator extends JFrame implements Runnable {
 		/////////////////////////////////////////////////////////////////
 		panel_1.add(txtHoraYFecha);
 		
-		JButton btnNewButton = new JButton("Cerrar Sesi\u00F3n");
-		btnNewButton.setBackground(UIManager.getColor("Button.highlight"));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				int resp = JOptionPane.showOptionDialog(null, "Estas seguro que deseas salir?", "Advertencia!", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[] { "Si", "No"}, null); 
-					if(resp == 0){
-						IniciarSesion aux =  new IniciarSesion();
-						aux.setVisible(true);
-						dispose();						
-					} else{
-							repaint();
-						}
-				
-				
-			}
-		});
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/stop_exit_close_6291.png")));
-		btnNewButton.setBounds(10, 376, 179, 57);
-		panel_1.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Tablero");
-		btnNewButton_1.setBackground(UIManager.getColor("Button.highlight"));
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton_1.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/controlPanel.png")));
-		btnNewButton_1.setBounds(10, 308, 179, 57);
-		panel_1.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Agenda");
-		btnNewButton_2.setBackground(UIManager.getColor("Button.highlight"));
-		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton_2.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/calendario.png")));
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton_2.setBounds(10, 240, 179, 57);
-		panel_1.add(btnNewButton_2);
-		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBackground(new Color(176, 196, 222));
 		panel_2.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		panel_2.setBounds(219, 11, 1111, 647);
+		panel_2.setBounds(219, 11, 1121, 618);
 		panel.add(panel_2);
 		panel_2.setLayout(null);
 		
-		JButton botonEmpleados = new JButton();
-		botonEmpleados.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/carpeta2.png")));
-		botonEmpleados.setText("EMPLEADOS");
-		botonEmpleados.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				VentanaSecre_admin aux = new VentanaSecre_admin();
-				aux.setVisible(true);
-			}
-		});
+		JPanel panelDeVacunas = new JPanel();
+		panelDeVacunas.setBackground(new Color(176, 196, 222));
+		panelDeVacunas.setBounds(21, 112, 528, 426);
+		panel_2.add(panelDeVacunas);
 		
+	    JPanel panelDeEnfermedades = new JPanel();
+	    panelDeEnfermedades.setBounds(570, 109, 528, 426);
+	    panel_2.add(panelDeEnfermedades);
+	    panelDeEnfermedades.setLayout(new BorderLayout(0, 0));
 		
-		botonEmpleados.setBackground(new Color(240, 248, 255));
-		botonEmpleados.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		botonEmpleados.setBounds(12, 594, 207, 42);
-		panel_2.add(botonEmpleados);
-		botonEmpleados.setLayout(null);
+////////////////////////////////////////////////GRAFICOS/////////////////////////////////////////////////////////
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 		
-		JButton panelPaciente = new JButton();
-		panelPaciente.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/agregrarpacientes2.png")));
-		panelPaciente.setText("PACIENTES");
-		panelPaciente.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				VentanaPacientes_admin aux = new VentanaPacientes_admin();
-				aux.setVisible(true);
-			}
-		});
-		panelPaciente.setBackground(new Color(240, 248, 255));
-		panelPaciente.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		panelPaciente.setBounds(231, 594, 207, 42);
-		panel_2.add(panelPaciente);
-		panelPaciente.setLayout(null);
-		
-		JButton panelEnfermedad = new JButton();
-		panelEnfermedad.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/Enfermedad.png")));
-		panelEnfermedad.setText("ENFERMEDADES");
-		panelEnfermedad.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				VentanaEnfermedad_admin aux = new VentanaEnfermedad_admin();
-				aux.setVisible(true);
-			}
-		});
-		panelEnfermedad.setBackground(new Color(240, 248, 255));
-		panelEnfermedad.setLayout(null);
-		panelEnfermedad.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		panelEnfermedad.setBounds(888, 594, 207, 42);
-		panel_2.add(panelEnfermedad);
-		
-		JButton panelVacuna = new JButton();
-		panelVacuna.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/vacuna.png")));
-		panelVacuna.setText("VACUNAS\r\n");
-		panelVacuna.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				VentanaVacuna_admin aux = new VentanaVacuna_admin();
-				aux.setVisible(true);
-			}
-		});
-		
-		JButton panelCitas = new JButton();
-		panelCitas.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/cita2.png")));
-		panelCitas.setText("CITAS");
-		panelCitas.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				VentanaCita_admin aux = new VentanaCita_admin();
-				aux.setVisible(true);
-			}
-		});
-		panelCitas.setBackground(new Color(240, 248, 255));
-		panelCitas.setLayout(null);
-		panelCitas.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		panelCitas.setBounds(450, 594, 207, 42);
-		panel_2.add(panelCitas);
-		panelCitas.setLayout(null);
-		
-		panelVacuna.setBackground(new Color(240, 248, 255));
-		panelVacuna.setLayout(null);
-		panelVacuna.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		panelVacuna.setBounds(669, 594, 207, 42);
-		panel_2.add(panelVacuna);
+      //EJEMPLO DE PRUEBA
+    	   dataset.setValue(8, "Vacunados", "Contra el Dengue");
+    	   dataset.setValue(4, "No Vacunados", "Contra el Dengue");
+    	   
+    	for(int i=0; i<Clinica.getInstance().getMisVacunas().size(); i++) { 
+    		
+    	 dataset.setValue(ContadorDeVacunas(Clinica.getInstance().getMisVacunas().get(i).getNombre_vacuna(), true), "Vacunados", Clinica.getInstance().getMisVacunas().get(i).getNombre_vacuna());
+    	 dataset.setValue(ContadorDeVacunas(Clinica.getInstance().getMisVacunas().get(i).getNombre_vacuna(), false), "No Vacunados", Clinica.getInstance().getMisVacunas().get(i).getNombre_vacuna());
+    	 
+    	}
+        // Creando el Grafico
+        JFreeChart chartVacunas = ChartFactory.createBarChart3D
+        ("Pacientes Vacunados","Vacunas", "Pacientes", dataset, PlotOrientation.VERTICAL, true,true, false);
+        chartVacunas.setBackgroundPaint(new Color(176, 196, 222));
+        chartVacunas.getTitle().setPaint(Color.black);
+        chartVacunas.getTitle().setFont(new Font("Tahoma", Font.BOLD, 12));
+        CategoryPlot p = chartVacunas.getCategoryPlot(); 
+        p.setRangeGridlinePaint(Color.red); 
+        // Mostrar Grafico
+        ChartPanel chartPanelVacunas = new ChartPanel(chartVacunas);
+        chartPanelVacunas.setBackground(Color.WHITE);
+        chartPanelVacunas.setBounds(100, 120, 200, 300);
+        panelDeVacunas.setLayout(new BorderLayout(0, 0));
+        panelDeVacunas.add(chartPanelVacunas);
+        
+ 
+    //////////////////////////////Grafico2///////////////////////////////////////////    
+        // Fuente de Datos
+        DefaultCategoryDataset datasetEnfermedades = new DefaultCategoryDataset();
+        
+        datasetEnfermedades.setValue(3, "Peligrosas", "Dengue");
+        datasetEnfermedades.setValue(10, "Normal", "Fiebre");
+        datasetEnfermedades.setValue(8, "Normal", "Diarrea");
+        datasetEnfermedades.setValue(20, "Peligrosas", "Colera");
+        
+        
+        /*for(int i = 0; i<Clinica.getInstance().getEnfermedades().size(); i++) {
+        	
+	        datasetEnfermedades.setValue(ContadorDeEfermedades(true), "Peligrosas", Clinica.getInstance().getEnfermedades().get(i).getNombre_enfermedad());
+	        datasetEnfermedades.setValue(ContadorDeEfermedades(false), "Normales", Clinica.getInstance().getEnfermedades().get(i).getNombre_enfermedad());
+
+        }*/
+ 
+        // Creando el Grafico
+        JFreeChart chartEnfermedades=ChartFactory.createBarChart3D("Estadisticas de Enfermedades", "Enfermedad", "Contagios", datasetEnfermedades, PlotOrientation.VERTICAL,true, true, false); 
+        chartEnfermedades.getTitle().setFont(new Font("Tahoma", Font.BOLD, 12));
+        chartEnfermedades.setBackgroundPaint(new Color(176, 196, 222));
+        CategoryPlot q = chartEnfermedades.getCategoryPlot(); 
+        q.setRangeGridlinePaint(Color.red); 
+        
+        // Mostrar Grafico
+        ChartPanel chartPanelEnfermedades = new ChartPanel(chartEnfermedades);
+        chartPanelEnfermedades.setSize(528, 426);
+        panelDeEnfermedades.add(chartPanelEnfermedades);
+        
+        JToolBar toolBar = new JToolBar();
+        contentPane.add(toolBar, BorderLayout.NORTH);
+        
+        JButton botonEmpleados = new JButton();
+        botonEmpleados.setFont(new Font("Tahoma", Font.BOLD, 11));
+        toolBar.add(botonEmpleados);
+        botonEmpleados.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/carpeta2.png")));
+        botonEmpleados.setText("Empleados   ");
+        botonEmpleados.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		VentanaSecre_admin aux = new VentanaSecre_admin();
+        		aux.setVisible(true);
+        	}
+        });
+        
+        
+        botonEmpleados.setBackground(UIManager.getColor("Button.highlight"));
+        botonEmpleados.setLayout(null);
+        
+        JButton panelPaciente = new JButton();
+        panelPaciente.setFont(new Font("Tahoma", Font.BOLD, 11));
+        toolBar.add(panelPaciente);
+        panelPaciente.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/agregrarpacientes2.png")));
+        panelPaciente.setText("Pacientes   ");
+        panelPaciente.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		VentanaPacientes_admin aux = new VentanaPacientes_admin();
+        		aux.setVisible(true);
+        	}
+        });
+        panelPaciente.setBackground(UIManager.getColor("Button.highlight"));
+        panelPaciente.setLayout(null);
+        
+        JButton panelCitas = new JButton();
+        panelCitas.setFont(new Font("Tahoma", Font.BOLD, 11));
+        toolBar.add(panelCitas);
+        panelCitas.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/cita2.png")));
+        panelCitas.setText("Citas    ");
+        panelCitas.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		VentanaCita_admin aux = new VentanaCita_admin();
+        		aux.setVisible(true);
+        	}
+        });
+        panelCitas.setBackground(UIManager.getColor("Button.highlight"));
+        panelCitas.setLayout(null);
+        panelCitas.setLayout(null);
+        
+        JButton panelVacuna = new JButton();
+        panelVacuna.setFont(new Font("Tahoma", Font.BOLD, 11));
+        toolBar.add(panelVacuna);
+        panelVacuna.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/vacuna.png")));
+        panelVacuna.setText("Vacunas    \r\n");
+        panelVacuna.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		VentanaVacuna_admin aux = new VentanaVacuna_admin();
+        		aux.setVisible(true);
+        	}
+        });
+        
+        panelVacuna.setBackground(UIManager.getColor("Button.highlight"));
+        panelVacuna.setLayout(null);
+        
+        JButton panelEnfermedad = new JButton();
+        panelEnfermedad.setFont(new Font("Tahoma", Font.BOLD, 11));
+        toolBar.add(panelEnfermedad);
+        panelEnfermedad.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/Enfermedad.png")));
+        panelEnfermedad.setText("Enfermedades    ");
+        panelEnfermedad.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		VentanaEnfermedad_admin aux = new VentanaEnfermedad_admin();
+        		aux.setVisible(true);
+        	}
+        });
+        panelEnfermedad.setBackground(UIManager.getColor("Button.highlight"));
+        panelEnfermedad.setLayout(null);
+        
+        JButton btnNewButton = new JButton("Cerrar Sesi\u00F3n  ");
+        toolBar.add(btnNewButton);
+        btnNewButton.setBackground(UIManager.getColor("Button.highlight"));
+        btnNewButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		
+        		int resp = JOptionPane.showOptionDialog(null, "Estas seguro que deseas salir?", "Advertencia!", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[] { "Si", "No"}, null); 
+        			if(resp == 0){
+        				IniciarSesion aux =  new IniciarSesion();
+        				aux.setVisible(true);
+        				dispose();						
+        			} else{
+        					repaint();
+        				}
+        		
+        		
+        	}
+        });
+        btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+        btnNewButton.setIcon(new ImageIcon(Administrator.class.getResource("/Imagenes/stop_exit_close_6291.png")));
 	}
 	
 
-////////////////////////////////////FUNCIONES PARA EL RELOJ//////////////////////////////
+
+
+	////////////////////////////////////FUNCIONES PARA EL RELOJ//////////////////////////////
 	@Override
 	public void run() {
 		Thread ct = Thread.currentThread();
@@ -311,4 +371,67 @@ public class Administrator extends JFrame implements Runnable {
         
         
     }
+	
+	public int ContadorDeVacunas(String NombreVacuna, boolean Return) { //TRUE = return Vacunados FALSE = return NO Vacunados
+		int Vacunados = 0;
+		int NOvacunados = 0;
+		boolean tiene = false;
+		
+		 for(int i = 0; i<Clinica.getInstance().getMisPersonas().size(); i++) {
+	    	   
+	    	   if (Clinica.getInstance().getMisPersonas().get(i) instanceof Paciente ) {
+	    		   
+	    		 for(int y = 0; y<((Paciente)Clinica.getInstance().getMisPersonas().get(i)).getMisVacunas().size(); y++) {
+	    			 
+		    		   if (((Paciente)Clinica.getInstance().getMisPersonas().get(i)).getMisVacunas().get(y).getNombre_vacuna().equalsIgnoreCase(NombreVacuna)) {
+		    			   Vacunados++;
+		    			   tiene = true;   
+		    		   }
+		    		   
+	    		  }
+	    		 	if(tiene == false) {
+	    			   NOvacunados++;
+	    		 	}
+	    		 	tiene = false;
+	    	   }
+	    	   	  
+		 }
+		 
+		if(Return) {
+		return Vacunados;
+		}else {
+		return NOvacunados;
+		}
+	}
+	
+	private int ContadorDeEfermedades(boolean Return) {
+		int EnfermedadesPeligrosas = 0;
+		int EnfermedadesNormales = 0;
+		
+		
+		 for(int i = 0; i<Clinica.getInstance().getMisPersonas().size(); i++) {
+	    	   
+	    	   if (Clinica.getInstance().getMisPersonas().get(i) instanceof Paciente ) {
+	    		   
+	    		 for(int y = 0; y<((Paciente)Clinica.getInstance().getMisPersonas().get(i)).getMisEnfermedas().size(); y++) {
+	    			 
+		    		   if (((Paciente)Clinica.getInstance().getMisPersonas().get(i)).getMisEnfermedas().get(y).isTipo_enfermedad()) {
+		    			  
+		    			   EnfermedadesPeligrosas++;  
+		    		   }else {
+		    			   EnfermedadesNormales++;
+		    		   }
+		    		   
+	    		  }
+	    		 	
+	    	   }
+	    	   	  
+		 }
+
+		if(Return) {
+			return EnfermedadesPeligrosas;
+		}else {
+			return EnfermedadesNormales;
+		}
+	}
 }
